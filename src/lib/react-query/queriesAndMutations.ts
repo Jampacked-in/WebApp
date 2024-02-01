a6a60c81ecff66e8a781987e56778711e3bcde78
Pranav <prsingh1347@gmail.com>
2024-02-01 15:30:13 +0530

    making-equal

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,9 +1,4 @@
-import {
-    useQuery,
-    useMutation,
-    useQueryClient,
-    useInfiniteQuery,
-} from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
 import { INewUser } from '@/types'
 import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 
